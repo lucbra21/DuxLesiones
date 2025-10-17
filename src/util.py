f282b8f4cecfa53b92af1b639bddd51c4ed3cac9
Jose Espinoza <espinozajgx@gmail.com>
2025-10-17 18:52:24 +0200

    df en inicio, nuevos campos en registro y nueva alta deportiva

--- src/util.py
+++ src/util.py
@@ -8,3 +8,38 @@ def get_photo(url):
         response = None  # Si hay un error, no asignamos nada a response
 
     return response
+
+def clean_df(records):
+    columnas_excluir = [
+    "id_jugadora",
+    "fecha_hora",
+    "posicion",
+    "tipo_tratamiento",
+    "diagnostico",
+    "descripcion",
+    "fecha",
+    "fecha_dia",
+    "evolucion",
+    "mecanismo_lesion",
+    "dias_baja_estimado",
+    "fecha_alta_lesion",
+    "fecha_alta_diagnostico",
+    "fecha_hora_registro",
+    "periodo"
+    ]
+    # --- eliminar columnas si existen ---
+    df_filtrado = records.drop(columns=[col for col in columnas_excluir if col in records.columns])
+
+    orden = ["fecha_lesion", "nombre_jugadora", "id_lesion", "lugar", "segmento", "zona_cuerpo", "zona_especifica", "lateralidad", "tipo_lesion", "tipo_especifico", "gravedad", "personal_reporta", "estado_lesion"]
+    
+    # Solo mantener columnas que realmente existen
+    orden_existentes = [c for c in orden if c in df_filtrado.columns]
+
+    df_filtrado = df_filtrado[orden_existentes + [c for c in df_filtrado.columns if c not in orden_existentes]]
+        
+    #df_filtrado = df_filtrado[orden + [c for c in df_filtrado.columns if c not in orden]]
+
+    df_filtrado = df_filtrado.sort_values("fecha_lesion", ascending=False)
+    df_filtrado.reset_index(drop=True, inplace=True)
+
+    return df_filtrado
